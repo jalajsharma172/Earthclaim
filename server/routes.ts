a2307cc43883b5c89d8897af15a42a33030479ca
jalajsharma172 <jalajsharma172@gmail.com>
2025-12-29 21:19:25 +0530

    testing vercel api

--- server/routes.ts
+++ server/routes.ts
@@ -123,6 +123,9 @@ export async function registerRoutes(app: Express): Promise<Server> {
   app.post("/api/dummy", async (req, res) => {
     return res.status(200).json({ success: true })
   })
+  app.get("/api/dummy", async (req, res) => {
+    return res.status(200).json({ success: true })
+  })
   app.all("/api/test-db", async (req, res) => {
     try {
       console.log("Testing Supabase Connection...");
